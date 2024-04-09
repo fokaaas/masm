ae90a28edc4cfbb3c1d363ef367866bfb48ae187
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-04-09 09:55:03 +0300

    added lab3 & lab4

---
+++ lab3/3-2-IM-22-Basarab-SAFE.asm
@@ -0,0 +1,80 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+include \masm32\include\masm32rt.inc
+
+.data
+	StanislavPasswordString			db "fYLGwTA", 0
+	StanislavKeyPasswordString		db "5--4552", 0
+	StanislavCorrectWindowTitle		db "Password is correct!", 0
+	StanislavCorrectWindowText		db "Full name of student: Basarab Stanislav Anatoliyovych", 13, 10,
+									"Date of birth: 07.04.2005", 13, 10,
+									"Zalik book: IM2202", 0
+	StanislavWrongWindowTitle		db "Wrong password", 0
+	StanislavWrongWindowText		db "Your password is wrong!", 0
+	
+.data?
+	StanislavFieldPasswordString	db 32	dup (?)
+	StanislavPasswordDecodedString	db 32	dup (?)
+
+.code
+
+Exit proc
+	invoke ExitProcess, 0
+Exit endp
+
+Handler proc WindowHandle:dword, Unit:dword, WordPar:dword, LonPar:dword
+	.if Unit == WM_COMMAND
+		.if WordPar == IDOK
+			invoke GetDlgItemText, WindowHandle, 1200, addr StanislavFieldPasswordString, 512
+			
+			MOV EBX, offset StanislavKeyPasswordString
+			MOV ESI, offset StanislavPasswordDecodedString
+			MOV EDI, offset StanislavFieldPasswordString
+			
+			cycl:
+				MOV AL, [EDI]
+				XOR AL, [EBX]
+				MOV [ESI], AL
+				INC EDI
+				INC EBX
+				INC ESI
+				CMP AL, 0
+				
+				.if AL != 0
+					JMP cycl
+				.endif
+			
+			MOV EAX, 0
+			
+			invoke lstrcmp, offset StanislavPasswordString, offset StanislavPasswordDecodedString
+			
+			.if EAX != 0
+				invoke MessageBox, 0, offset StanislavWrongWindowText, offset StanislavWrongWindowTitle, 0
+			.else
+				invoke MessageBox, 0, offset StanislavCorrectWindowText, offset StanislavCorrectWindowTitle, 0
+			.endif
+			
+		.endif
+		
+		.if WordPar == IDCANCEL
+			call Exit
+		.endif
+			
+	.elseif Unit == WM_CLOSE
+		call Exit
+	.endif
+	return 0
+Handler endp
+
+start:
+	Dialog "Lab3 | SAFE", "Tahoma", 11, \
+	WS_SYSMENU or WS_OVERLAPPED or DS_CENTER, 4, 20, 20, 120, 95, 4466
+	DlgStatic "Put the password", SS_CENTER, 1, 20, 75, 12, 445
+	DlgEdit WS_BORDER, 6, 30, 110, 12, 1200
+	DlgButton "Cancel", WS_TABSTOP, 19, 42, 35, 20, IDCANCEL 
+	DlgButton "OK", WS_TABSTOP, 78, 42, 35, 20, IDOK
+	
+	CallModalDialog 0, 0, Handler, NULL
+end start
---
+++ lab4/4-2-IM-22-Basarab-INC.asm
@@ -0,0 +1,65 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+include 4-2-IM-22-Basarab.inc
+include \masm32\include\masm32rt.inc
+
+.data?
+	BasarabPasswordBuffer		db 16	dup (?)
+	BasarabDecryptedPassword	db 16	dup (?)
+
+.data
+	BasarabSuccessWindowCaption		db "The password is entered correctly!", 0
+	BasarabFullNameWindowMessage	db "Full name: Basarab Stanislav Anatoliyovych", 0
+	BasarabBirthDateWindowMessage	db "Date of Birth: 07.04.2005", 0
+	BasarabStudentIdWindowMessage	db "Student ID: IM2202", 0
+								
+	BasarabErrorWindowCaption		db "Error", 0
+	BasarabErrorWindowMessage		db "The password is incorrect, please try again!", 0
+	
+	BasarabPassword					db "kGXFrSD", 0
+	BasarabPasswordKey				db "8395027", 0
+
+.code
+
+Handle proc
+	ComparePassword BasarabDecryptedPassword, BasarabPassword
+	.if eax == 0
+		ShowWindowMessage BasarabFullNameWindowMessage, BasarabSuccessWindowCaption
+		ShowWindowMessage BasarabBirthDateWindowMessage, BasarabSuccessWindowCaption
+		ShowWindowMessage BasarabStudentIdWindowMessage, BasarabSuccessWindowCaption
+	.else
+		ShowWindowMessage BasarabErrorWindowMessage, BasarabErrorWindowCaption
+	.endif
+	ret
+Handle endp
+
+DialogProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
+	.if uMsg == WM_COMMAND
+		.if wParam == IDCANCEL
+			invoke ExitProcess, 0
+			
+		.elseif wParam == IDOK
+			invoke GetDlgItemText, hWnd, 100, addr BasarabPasswordBuffer, 512
+			DecryptPassword
+			call Handle
+		.endif
+		
+	.elseif uMsg == WM_CLOSE
+		invoke ExitProcess, 0
+	.endif
+	
+	return 0
+DialogProc endp
+
+start:
+	Dialog "Lab. work 4. Stanislav Basarab IM-22", "Tahoma", 11, \
+	WS_SYSMENU or WS_OVERLAPPED or DS_CENTER, 4, 15, 15, 130, 100, 2202
+	DlgStatic "Enter the password", SS_CENTER, 0, 15, 80, 10, 123
+	DlgEdit WS_BORDER, 5, 25, 100, 10, 100
+	DlgButton "Cancel", WS_TABSTOP, 20, 40, 30, 15, IDCANCEL 
+	DlgButton "Confirm", WS_TABSTOP, 75, 40, 30, 15, IDOK
+	
+	CallModalDialog 0, 0, DialogProc, NULL
+end start
